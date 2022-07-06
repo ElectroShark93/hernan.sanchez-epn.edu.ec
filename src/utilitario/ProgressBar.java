0985b8bf8a3aee6d0de2bf3fd2192d1cae891c8c
Hernan Sanchez <herez2002@gmail.com>
2022-07-06 16:54:03 -0500

    cambios2

--- src/utilitario/ProgressBar.java
+++ src/utilitario/ProgressBar.java
@@ -11,24 +11,24 @@ public class ProgressBar {
     public static void showProgressBar_e(String nombre,String c) throws IOException, InterruptedException{
         int TamBar = 30;
         String sb = new String();
-        
+        for (int i = 0; i <= nombre.length(); i++) {
             for (int procentaje = 0; procentaje <= 100; procentaje++) {
-                procentaje+=3;            
-                int nroCaracteres = procentaje * TamBar / 100;  //numero de caracteres
-                sb =  "\r [";
-                for (int k = 0; k <= TamBar; k++){
-                    sb += String.format("%s", ((nroCaracteres < k) ? " " : c.charAt(procentaje % c.length()))  );
-                }
-                sb += "]";
                 
-                System.out.write(sb.getBytes());
-                System.out.printf(" %d %%", procentaje,nombre.length());
-                for (int i = 0; i <= nombre.length(); i++) {
-                
-                System.out.println("\t Loading: " + i + " de 25 \t " + nombre.charAt(i++) );
+                    procentaje+=2;            
+                    int nroCaracteres = procentaje * TamBar / 100;  //numero de caracteres
+                    sb =  "\r [";
+                    for (int k = 0; k <= TamBar; k++){
+                        sb += String.format("%s", ((nroCaracteres < k) ? " " : c.charAt(procentaje % c.length()))  );
+                    }
+                    sb += "]";
+                    
+                    System.out.write(sb.getBytes());
+                    System.out.printf(" %d %%", procentaje,nombre.length());
+                    System.out.println("\t Loading: " + i + " de 25 \t " + nombre.charAt(i++) );
             }
             
-            Thread.sleep(300);
+            try { Thread.sleep(100); }
+            catch (Exception e) {}
             
         }
         
